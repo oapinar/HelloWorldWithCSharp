e5cc3ee50e64b23e861d6671d8af5223c63d51ca
oapinar <ozerardapinar@gmail.com>
2024-05-17 19:47:46 +0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"\nHello {name}, on {currentDate:d} at {currentDate:t}");
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
